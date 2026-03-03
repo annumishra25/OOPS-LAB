dd520e69b01312d4c5642392b8c4e556d80d54a5
annumishra25 <annu16730@gmail.com>
2026-03-03 20:39:43 +0530

    Add files via upload

---
+++ Modifiers/Access Modifiers/Annu1.java
@@ -0,0 +1,12 @@
+class Annu1{
+    public String rollno="CH.SC.U4CSE25004";
+    public String name="Annu Mishra";
+
+}
+public class Exp_1 {
+    public static void main(String[] args) {
+        Annu1 n = new Annu();
+        System.out.println("Roll.no:"+n.rollno);
+        System.out.println("Name:"+n.name);
+    }
+}
